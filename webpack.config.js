533958358077c0dce530d0919048c85c7d06f5b3
lfyfly <410793635@qq.com>
2020-04-22 20:31:14 +0800

    通过css-loader style-laoder 加载 css文件

---
+++ webpack.config.js
@@ -0,0 +1,17 @@
+
+module.exports = {
+  mode: 'none',
+  entry: './src/index.js',
+  output: {
+    filename: 'bundle.js',
+  },
+  module:{
+    rules:[
+      {
+        test: /.css$/,
+        // 多loader从后往前执行
+        use: ['style-loader','css-loader'] // css-loader只是吧css转化成js模块，而style-loader才是将css放到style标签中，插入页面
+      }
+    ]
+  }
+}
