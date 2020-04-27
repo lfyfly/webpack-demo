d1dd5d42525992c4f0a7ed4d12581606145a49bf
lfyfly <410793635@qq.com>
2020-04-27 18:35:52 +0800

    注释console

--- my-loader/markdown-loader.js
+++ my-loader/markdown-loader.js
@@ -1,7 +1,7 @@
 const markded = require('marked')  
 
 module.exports = sorce=>{
-  console.log(sorce)
+  // console.log(sorce)
   const html =  markded(sorce)
   // loader 输出必须是一段js代码字符串
   return `export default ${JSON.stringify(html)}`
